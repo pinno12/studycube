8d63aaf8d9b33badac4a188f67bb25297cd202eb
wonny <pinno12@kaist.ac.kr>
2021-03-18 20:28:20 +0900

    update navbar

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ $('.ui.modal')
   .modal('show')
 ;
 
-
-
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
   
 $('.tool-tip').popup();
 
