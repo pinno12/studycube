dcd2ca9453fa50102dbf700d8172367b5600d58a
wonny <pinno12@kaist.ac.kr>
2021-03-30 19:12:42 +0900

    refactoring

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-
+//범용 Semantic ui 사용
 $('.ui.modal')
   .modal('show')
 ;
 
-// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//   return new bootstrap.Tooltip(tooltipTriggerEl)
-// })
-  
 $('.tool-tip').popup();
 
-function bg_change(color) {
-  $(".ui.button.full-width.items").removeClass("bg-e");
-  document.getElementById(color).classList.add("bg-e");
-  document.getElementById("item-name").textContent = color;
-  document.getElementById("item-name2").textContent = color;
-}
 
-function smartkey(item) {
-  item = item || "2003051"; // default
+$('.ui.checkbox')
+  .checkbox()
+  ;
 
-  $(".item").removeClass("active");
-  document.getElementById(item).classList.add("active");
-  document.getElementById('myQr_' + item).classList.add("d-none");
-  
-  $("#selected").empty();
-  $("#" + item).clone().appendTo("#selected")
+  $('.menu .item')
+  .tab()
+;
 
-  document.getElementById('myQr_' + item).classList.remove("d-none");
-}
+$('.accordion')
+  .accordion({
+    selector: {
+      trigger: '.title'
+    }
+  })
+;
 
+//-- Index 예약 홈 --//
 
-select_first = (item) => {
-  document.getElementById("select_first").textContent  = document.getElementById(item).innerText ;  
+// 좌석 선택
+select_seat = (item) => {
+  document.getElementById("select_seat").textContent  = document.getElementById(item).innerText ;  
   document.getElementById("show_cal").classList.remove("d-none");
 }
 
-
+// 시간 선택
 select_hour = (item) => {
   document.getElementById("select_hour").textContent  = document.getElementById(item).innerText;
   document.getElementById("show_seat").classList.remove("d-none"); 
@@ -49,21 +43,20 @@ select_times = (item) => {
   show_pay_box();
 }
 
+// 날짜 선택
 select_days = (item) => {
   document.getElementById("_days_added").textContent  = item;  
   document.getElementById("_days_t").classList.remove("d-none"); 
   show_pay_box();
 }
 
-
+// 결제창 보이기
 show_pay_box = () =>{
   document.getElementById("pay-box").classList.remove("d-none");
 }
 
 show_pays = (item) =>{
   document.getElementById("pills-tabContent").classList.remove("d-none");
-  
-
   switch (item){
     case 'pills-home-tab':      
       document.getElementById("time_pay").style.display = "block";
@@ -92,53 +85,29 @@ show_pays = (item) =>{
         document.getElementById("_times").style.display = "none";
         document.getElementById("_days_t").style.display = "none";
         document.getElementById("pay_locker").style.display = "block";
-  
-    }
+    }   
+}
 
-
-  // if (item == 'pills-home-tab'){
-  //   document.getElementById("time_pay").style.display = "block";
-  // }else{
-  //   document.getElementById("time_pay").style.display = "none";
-  // }
-
-  // document.getElementById("pay-detail-s").innerHTML = ""
-  // document.getElementById("_times").innerHTML = ""
-  // document.getElementById("_days_t").innerHTML = ""
-    
-  }
-
+// 사물함
 select_weeks_locker = (item) => {
   document.getElementById("_locker_weeks").textContent  = item;
   document.getElementById("locker-cal").classList.remove("d-none");
 }
 
+
+//// -----가입 join------////
+// 인증번호 카톡 확인 안내
 confirm_kakao_msg = () =>{
   document.getElementById("confirm_msg").classList.remove("d-none");
 }
   
 
-function uncheck() {
+function uncheck(){
   document.getElementById("red").checked = false;
 }
 
 
-$('.ui.checkbox')
-  .checkbox()
-  ;
-
-  $('.menu .item')
-  .tab()
-;
-
-$('.accordion')
-  .accordion({
-    selector: {
-      trigger: '.title'
-    }
-  })
-;
-
+// 핸드폰 번호에 "-" 짝대기 자동으로 넣기
 window.addDashes = function addDashes(f) {
   var r = /(\D+)/g,
       npa = '',
@@ -149,7 +118,6 @@ window.addDashes = function addDashes(f) {
   nxx = f.value.substr(3, 4);
   last4 = f.value.substr(7, 4);
   f.value = npa + '-' + nxx + '-' + last4;
-
 }
 
 add_dashes = (item) =>{
